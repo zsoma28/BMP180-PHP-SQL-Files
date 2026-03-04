28cd8a0e7c9dff67e6a342726e57a0887a8e8a43
zsoma28 <54069252+zsoma28@users.noreply.github.com>
2026-03-04 08:18:44 +0100

    Update insert_data.php

--- insert_data.php
+++ insert_data.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username   = "root";          // saját adatbázis user
 $password   = "";              // saját jelszó
-$dbname     = "BMP-180";
+$dbname     = "BMP180";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
